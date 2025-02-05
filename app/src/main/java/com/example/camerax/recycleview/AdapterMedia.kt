71a28ed378042ab347ab7c685c749c2123a8bd9f
Danil Valihmedov <valiahmedov2013@mail.ru>
2025-02-05 19:13:26 +0300

    Fixed a bug with displaying a file with a different media type instead of non-existent elements in a row when switching from one file type to another in the gallery. Added the player icon to the video in the gallery

--- app/src/main/java/com/example/camerax/recycleview/AdapterMedia.kt
+++ app/src/main/java/com/example/camerax/recycleview/AdapterMedia.kt
@@ -16,7 +16,6 @@ import com.example.camerax.fragments.ViewingVideosFragment
 class AdapterPhoto(private val context: Context,
                    private val fragmentManager: FragmentManager,
                    private var dataList: List<RecyclerMediasThree>): RecyclerView.Adapter<AdapterPhoto.MediaViewHolder>() {
-
     fun updateList(newList: List<RecyclerMediasThree>) {
         dataList = ArrayList(newList)
         notifyDataSetChanged()
@@ -30,14 +29,21 @@ class AdapterPhoto(private val context: Context,
     override fun onBindViewHolder(holder: MediaViewHolder, position: Int) {
         val item = dataList[position]
 
-        launching(item.media1?.uri, item.media1?.type, holder.image1)
-        launching(item.media2?.uri, item.media2?.type, holder.image2)
-        launching(item.media3?.uri, item.media3?.type, holder.image3)
+        launching(item.media1?.uri, item.media1?.type, holder.image1, holder.playIcon1)
+        launching(item.media2?.uri, item.media2?.type, holder.image2, holder.playIcon2)
+        launching(item.media3?.uri, item.media3?.type, holder.image3, holder.playIcon3)
     }
 
-    private fun launching(itemUri: String?, itemType: MediaType?, holderImage: ImageView){
+    private fun launching(
+        itemUri: String?, itemType: MediaType?, holderImage: ImageView, holderPlayIcon: ImageView
+    ){
+        //Очистка элемента при переключении с одного типа файлов на другой
+        holderImage.setImageDrawable(null)
+        holderPlayIcon.visibility = View.GONE
+
         if(itemUri != null && itemType != null) {
             Glide.with(context).load(itemUri).into(holderImage)
+            if(itemType == MediaType.VIDEO)holderPlayIcon.visibility = View.VISIBLE
 
             val fragment = when(itemType) {
                 MediaType.PHOTO -> ViewingPhotosFragment.newInstance(itemUri, true)
@@ -63,5 +69,8 @@ class AdapterPhoto(private val context: Context,
         val image1: ImageView = itemView.findViewById(R.id.image1)
         val image2: ImageView = itemView.findViewById(R.id.image2)
         val image3: ImageView = itemView.findViewById(R.id.image3)
+        val playIcon1: ImageView = itemView.findViewById(R.id.playIcon1)
+        val playIcon2: ImageView = itemView.findViewById(R.id.playIcon2)
+        val playIcon3: ImageView = itemView.findViewById(R.id.playIcon3)
     }
 }
